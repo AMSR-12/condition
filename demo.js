e3a13a21dcb330ba2c7a86d39e5b0aa7089a0813
AMSR-12 <meenukutty30399@gmail.com>
2024-05-13 14:46:22 +0530

    condition created

---
+++ demo.js
@@ -0,0 +1,28 @@
+// var factor='vijay';
+// var fplayer='dhoni';
+// var fmovie=3;
+// function fff()
+// {
+//     console.log('Favourite Actor', ':' + factor);
+//     console.log('Favourite player', ':' + fplayer)
+//     console.log('Favourite movie', ':' + fmovie);
+// }
+// fff();
+// var signal='green'
+
+// if(signal=='yellow')
+// {
+//     console.log('get ready')
+// }
+// else if(signal=='red')
+// {
+//     console.log('stop')
+// }
+// else{
+//     console.log('go')
+// }
+// 
+//const x = 1 -1 + "1";
+//console.log(x);
+
+// 
